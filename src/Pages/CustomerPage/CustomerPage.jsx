916a56bed4fba288fe979c920fc8eb3f063c3f90
Levis155 <levismbui@gmail.com>
2025-05-14 14:17:07 +0300

    build the rate and review form

--- src/Pages/CustomerPage/CustomerPage.jsx
+++ src/Pages/CustomerPage/CustomerPage.jsx
@@ -18,6 +18,7 @@ import {
   IconButton,
   OutlinedInput,
   InputAdornment,
+  Rating,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
@@ -62,6 +63,7 @@ function CustomerPage() {
           <EditPassword />
           <OrderedProducts />
           <ReviewProducts />
+          <RateAndReviewForm />
         </div>
       </div>
     </>
@@ -128,7 +130,7 @@ function EditAccountDetails() {
           variant="outlined"
           sx={{
             "& .MuiInputBase-input": {
-              fontSize: "1.6rem", // Input text size
+              fontSize: "1.6rem",
             },
             "& .MuiInputLabel-root": {
               fontSize: "1.2rem",
@@ -447,6 +449,101 @@ function ProductToReview() {
         <Link>rate this product</Link>
       </div>
     </div>
+  );
+}
+
+function RateAndReviewForm() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <form action="">
+      <p className="rate-and-review-form-title">rate & review</p>
+
+      <div className="rate-and-review-form-body">
+        <div className="rate-container">
+          <p className="rate-container-title">
+            select the stars to rate the product
+          </p>
+
+          <div className="rate-container-card">
+            <div className="rate-container-card-col1">
+              <div className="rate-container-card-img">
+                <img src="src\assets\jbl-live.png" alt="" />
+              </div>
+            </div>
+
+            <div className="rate-container-card-col2">
+              <p className="rate-container-card-product-headline">
+                Jbl Tune 520 Wireless Over-Ear Bluetooth Headphones, Pure
+                Bass,Foldable, Long-Lasting Battery
+              </p>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="review-container">
+          <p className="review-container-title">leave a review</p>
+
+          <div className="review-container-body">
+            <div className="text-field-container">
+              <TextField
+                id="outlined-basic-1"
+                label="Review Title"
+                variant="outlined"
+                sx={{
+                  "& .MuiInputBase-input": {
+                    fontSize: "1.6rem",
+                  },
+                  "& .MuiInputLabel-root": {
+                    fontSize: "1.2rem",
+                  },
+                }}
+              />
+
+              <TextField
+                id="outlined-basic-1"
+                label="Your Name"
+                variant="outlined"
+                sx={{
+                  "& .MuiInputBase-input": {
+                    fontSize: "1.6rem",
+                  },
+                  "& .MuiInputLabel-root": {
+                    fontSize: "1.2rem",
+                  },
+                }}
+              />
+            </div>
+
+            <div className="text-area-container">
+              <TextField
+                id="outlined-multiline-static"
+                label="Detailed Review"
+                multiline
+                rows={7}
+                // defaultValue="Default Value"
+                sx={{
+                  "& .MuiInputBase-input": {
+                    fontSize: "1.6rem",
+                  },
+                  "& .MuiInputLabel-root": {
+                    fontSize: "1.2rem",
+                  },
+                }}
+              />
+            </div>
+          </div>
+        </div>
+        <button className="submit-review-btn">submit you review</button>
+      </div>
+    </form>
   );
 }
 
